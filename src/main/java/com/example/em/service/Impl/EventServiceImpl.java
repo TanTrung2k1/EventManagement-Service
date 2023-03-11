4403f534e01d72dbedd8aa54098d7fe40f028145
taolaprovip <tranthequanganime@gmail.com>
2023-03-11 13:01:31 +0700

    -create, view, delete event

--- src/main/java/com/example/em/service/Impl/EventServiceImpl.java
+++ src/main/java/com/example/em/service/Impl/EventServiceImpl.java
@@ -2,8 +2,12 @@ package com.example.em.service.Impl;
 
 import com.example.em.dto.event.CEventDTO;
 import com.example.em.dto.listEvent.EventDTO;
+import com.example.em.dto.response.ManagerLoginDTO;
+import com.example.em.dto.response.UserLoginDTO;
 import com.example.em.entity.Event;
+import com.example.em.entity.EventBooking;
 import com.example.em.entity.Manager;
+import com.example.em.entity.User;
 import com.example.em.repository.EventRepository;
 import com.example.em.repository.ManagerRepository;
 import com.example.em.service.IEventService;
@@ -11,8 +15,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class EventServiceImpl implements IEventService {
@@ -33,19 +39,36 @@ public class EventServiceImpl implements IEventService {
         return result;
     }
 
+    private Manager getManagerInSession(HttpSession session){
+        ManagerLoginDTO managerDTO = (ManagerLoginDTO) session.getAttribute("MA");
+        Manager manager = managerRepo.getReferenceById(managerDTO.getId());
+        return manager;
+    }
     @Override
-    public CEventDTO addEvent(CEventDTO eventDTO) {
-        //manager lay tu userlogin
-        long id = 16;
-        Manager manager = getManagerById(id);
+    public CEventDTO addEvent(CEventDTO eventDTO, HttpSession session) {
 
-        //
+        Manager manager = getManagerInSession(session);
         Event event = modelMapper.map(eventDTO, Event.class);
         event.setManager(manager);
         event.setStatus(true);
         repo.save(event);
+
         return modelMapper.map(event, CEventDTO.class);
     }
+
+    @Override
+    public Boolean deleteEventById(Long id) {
+        boolean result = false;
+        Optional<Event> optionalEvent = repo.findById(id);
+        if(optionalEvent.isPresent()){
+            Event event = optionalEvent.get();
+            event.setStatus(false);
+            repo.save(event);
+            result = true;
+        }
+        return result;
+    }
+
     private Manager getManagerById(Long id){
         return managerRepo.getReferenceById(id);
     }
