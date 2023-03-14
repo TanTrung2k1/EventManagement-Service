8befc2ce197574763c36f4cea2ac62a59905a222
chuqdu <quangdu2601@gmail.com>
2023-03-14 10:03:45 +0700

    Resolve author class merge conflict

--- src/main/java/com/example/em/config/Author.java
+++ src/main/java/com/example/em/config/Author.java
@@ -3,11 +3,9 @@ package com.example.em.config;
 import com.example.em.dto.response.AdminLoginDTO;
 import com.example.em.dto.response.ManagerLoginDTO;
 import com.example.em.dto.response.UserLoginDTO;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
 
-@Component
 public class Author {
     public static Boolean isAuthorOfAdmin(HttpSession session){
         boolean result = false;
