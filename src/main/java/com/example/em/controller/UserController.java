33b10ff5a7354930beec72c0cdc1fae4260ac7f1
chuqdu <quangdu2601@gmail.com>
2023-03-14 08:24:09 +0700

    Add User controllers, services and repository

--- src/main/java/com/example/em/controller/UserController.java
+++ src/main/java/com/example/em/controller/UserController.java
@@ -1,10 +1,70 @@
 package com.example.em.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.em.config.Author;
+import com.example.em.dto.response.ResponseObject;
+import com.example.em.dto.user.CreateUserDTO;
+import com.example.em.dto.user.UserDTO;
+import com.example.em.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    @Autowired
+    private IUserService userService;
 
+    @Autowired
+    private Author authorizeService;
+
+    @GetMapping
+    public ResponseEntity<ResponseObject> getAll(HttpSession httpSession) {
+        List<UserDTO> result = userService.getAll();
+        return ResponseEntity.status(HttpStatus.OK)
+                             .body(new ResponseObject(HttpStatus.OK.toString(), "List user", result));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseObject> getUserById(HttpSession httpSession, @PathVariable Long id) {
+        UserDTO result = userService.getUserById(id);
+        if(result != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .body(new ResponseObject(HttpStatus.OK.toString(), "User details", result));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ResponseObject(HttpStatus.NOT_FOUND.toString(), "User with ID " + id + " not found", null));
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseObject> create(@RequestBody CreateUserDTO createUserDTO) {
+        UserDTO result = userService.addUser(createUserDTO);
+        if(result != null) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                                 .body(new ResponseObject(HttpStatus.CREATED.toString(), "User created successfully", result));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                 .body(new ResponseObject(HttpStatus.BAD_REQUEST.toString(), "User with name " + createUserDTO.getName() + " already exist", null));
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<ResponseObject> deleteById(HttpSession httpSession, @PathVariable Long id) {
+        boolean isDeleted = userService.deleteUserById(id);
+        if(isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .body(new ResponseObject(HttpStatus.OK.toString(), "User with ID " + id + " deleted successfully", null));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                 .body(new ResponseObject(HttpStatus.NOT_FOUND.toString(), "User with ID " + id + " not found", null));
+        }
+    }
 }
--- src/main/java/com/example/em/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/em/service/Impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.em.service.Impl;
 
 import com.example.em.dto.response.UserLoginDTO;
+import com.example.em.dto.user.CreateUserDTO;
+import com.example.em.dto.user.UserDTO;
 import com.example.em.entity.User;
 import com.example.em.repository.UserRepository;
 import com.example.em.service.IUserService;
@@ -8,22 +10,65 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Component
 public class UserServiceImpl implements IUserService {
     @Autowired
-    private UserRepository userRepo;
+    private UserRepository userRepository;
+
     @Autowired
     private ModelMapper modelMapper;
+
     @Override
     public UserLoginDTO checkLogin(String email, String password) {
         UserLoginDTO result = null;
-        Optional<User> optionalUser = userRepo.findByEmailAndPassword(email, password);
+        Optional<User> optionalUser = userRepository.findByEmailAndPassword(email, password);
         if(optionalUser.isPresent()){
             User user = optionalUser.get();
             result = modelMapper.map(user, UserLoginDTO.class);
         }
         return result;
+    }
+
+    @Override
+    public List<UserDTO> getAll() {
+        List<User> userList = userRepository.findByStatusTrue();
+        List<UserDTO> userDTOList = new ArrayList<>();
+        if(!userList.isEmpty()) {
+            for(User user: userList) {
+                userDTOList.add(modelMapper.map(user, UserDTO.class));
+            }
+        }
+        return userDTOList;
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        Optional<User> userFetchResult = userRepository.findByIdAndStatusTrue(id);
+        return userFetchResult.map(user -> modelMapper.map(user, UserDTO.class)).orElse(null);
+    }
+
+    @Override
+    public UserDTO addUser(CreateUserDTO createUserDTO) {
+        User user = modelMapper.map(createUserDTO, User.class);
+        if(userRepository.findByNameAndStatusTrue(user.getName()).isEmpty()) {
+            user.setStatus(true);
+            return modelMapper.map(userRepository.save(user), UserDTO.class);
+        }
+        return null;
+    }
+
+    public Boolean deleteUserById(Long id) {
+        Optional<User> fetchResult = userRepository.findByIdAndStatusTrue(id);
+        if(fetchResult.isPresent()) {
+            User deletedUser = fetchResult.get();
+            deletedUser.setStatus(false);
+            userRepository.save(deletedUser);
+            return true;
+        }
+        return false;
     }
 }
